c23e00554bc951054a934ce0a8bf7843ba5b31f6
soufu <c1417005@st.kanazawa-it.ac.jp>
2026-05-20 22:35:03 +0900

    クラスについて学習

--- class.ts
+++ class.ts
@@ -1,25 +1,49 @@
-class Person{
-    name:string;
-    constructor(initName:string){
-        this.name = initName;
+abstract class Person{
+    static species ='Homo sapiens';
+    static isAdalt(age: number){
+        if(age>17) return true;
+        return false;
+    }
+    constructor(public readonly name :string, protected age: number){
         // コンストラクタ内で呼び出されたthisはこのクラスそのもの。
     }
-    greeting(){
-        console.log(`Hello My name is ${this.name}`);
+
+    incrementAge(){
+        this.age += 1;
+        Person.isAdalt(this.age);
+    }
+
+    greeting(this:Person){
+        console.log(`Hello My name is ${this.name}.I'm ${this.age} years old`);
+        this.explainjob();
 
     }
+    abstract explainjob(): void;
 }
 
-const quill = new Person('Quill');
-// console.log(quill);
+class Teacher extends Person {
+    explainjob(): void {
+        console.log('I am a teacher.')
+    }
+    get subject():string {
+        if(!this._subject){
+            throw new Error('There is no subject.')
+        }
+        return 'Music'
+    }
 
-quill.greeting();
-// オブジェクトのメソッドで呼ばれたthisは、呼び出したオブジェクトを指す。
+    set subject(value){
+        if(!value){
+            throw new Error('There is no value')
+        }
+        this._subject = value;
+    }
+    constructor(name: string, age: number, private _subject: string){
+        // 継承先で登場した新しい引数にはprivate等をつける
+        super(name,age);
+        // superには継承元の初期化に必要な引数をかく
+    }
 
-const anotherQuill = {
-    // name: 'anotherQUill',
-    anotherGreeting: quill.greeting
 }
 
-anotherQuill.anotherGreeting();
-// 呼び出し元のanotherQuilにnameがないので、この場合undefined
+console.log(Person.isAdalt(67));
